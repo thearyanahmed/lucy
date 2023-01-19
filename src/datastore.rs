7bbb9ccea4a8f934fb987497a0f3c6f8cb04eeeb
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2023-01-19 12:56:41 +0100

    Add enum for available datastore drivers

--- src/datastore.rs
+++ src/datastore.rs
@@ -1,8 +1,17 @@
 use crate::record::Record;
 use crate::error::LucyError;
 
+pub enum DatastoreDriver {
+    InMemoryHashmap,
+    // MySQL,
+    // PostgreSQL,
+    // Redis,
+}
+
 pub trait Datastore {
     fn find(&self, uuid: &str) -> Result<Record, LucyError>;
 
     fn record(&mut self, record: Record) -> Result<bool, String>;
+
+    fn all(&self) -> Vec<Record>;
 }
